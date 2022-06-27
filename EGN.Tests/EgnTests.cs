0e74f814d3aebba7ea48ffd8723f7764ebd8aa71
Banovvv <ivan_banov@abv.bg>
2022-06-27 19:43:18 +0300

    More extensive tests

--- EGN.Tests/EgnTests.cs
+++ EGN.Tests/EgnTests.cs
@@ -7,7 +7,7 @@ namespace EGN.Tests
     public class EgnTests
     {
         [Test]
-        [TestCase("9206183047")]
+        [TestCase("9206183026")]
         [TestCase("7524169268")]
         [TestCase("7501010010")]
         public void ValidateShouldWorkWithValidEgn(string egn)
@@ -31,6 +31,30 @@ namespace EGN.Tests
         [TestCase("acbd1234xx")]
         [TestCase("qwaserasdx")]
         public void ValidateShouldNotWorkWithNonNumericEgn(string egn)
+        {
+            var egnValidator = new Egn();
+            var result = egnValidator.Validate(egn);
+            Assert.False(result);
+        }
+
+        [Test]
+        [TestCase("92061830")]
+        [TestCase("7523")]
+        [TestCase("acbd1234")]
+        [TestCase("qwaserast")]
+        public void ValidateShouldNotWorkWithShorterEgn(string egn)
+        {
+            var egnValidator = new Egn();
+            var result = egnValidator.Validate(egn);
+            Assert.False(result);
+        }
+
+        [Test]
+        [TestCase("9206183026912")]
+        [TestCase("75234145111123312")]
+        [TestCase("acbd12331dadaada4")]
+        [TestCase("qwasedddddadarasdada123t")]
+        public void ValidateShouldNotWorkWithLongerEgn(string egn)
         {
             var egnValidator = new Egn();
             var result = egnValidator.Validate(egn);
